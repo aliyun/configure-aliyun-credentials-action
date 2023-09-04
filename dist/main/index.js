293b1c958b37cefeaa805a5157dda952c63ebad0
Jackson Tian <shyvo1987@gmail.com>
2023-09-04 20:48:47 +0800

    Bump 1.0.0

--- dist/main/index.js
+++ dist/main/index.js
@@ -6257,6 +6257,14 @@ module.exports = require("fs");
 
 /***/ }),
 
+/***/ 3292:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("fs/promises");
+
+/***/ }),
+
 /***/ 3685:
 /***/ ((module) => {
 
@@ -6403,7 +6411,12 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be in strict mode.
 (() => {
 "use strict";
+var exports = __webpack_exports__;
 
+
+const os = __nccwpck_require__(2037);
+const path = __nccwpck_require__(1017);
+const fsx = __nccwpck_require__(3292);
 
 const core = __nccwpck_require__(2186);
 const acc = __nccwpck_require__(4060);
@@ -6411,68 +6424,81 @@ const acc = __nccwpck_require__(4060);
 const CredentialClient = acc.default;
 const Config = acc.Config;
 
+const ROLE_SESSION_NAME = 'github-action-session';
+
 function setOutput(accessKeyId, accessKeySecret, securityToken) {
-    core.setOutput("aliyun-access-key-id", accessKeyId);
-    core.setOutput("aliyun-access-key-secret", accesskeySecret);
-    core.setOutput("aliyun-security-token", securityToken);
-    core.exportVariable('ALIBABACLOUD_ACCESS_KEY_ID', accessKeyId);
-    core.exportVariable('ALIBABACLOUD_ACCESS_KEY_SECRET', accessKeySecret);
-    core.exportVariable('ALIBABACLOUD_SECURITY_TOKEN', securityToken);
+  core.setOutput('aliyun-access-key-id', accessKeyId);
+  core.setOutput('aliyun-access-key-secret', accessKeySecret);
+  core.setOutput('aliyun-security-token', securityToken);
+  core.exportVariable('ALIBABACLOUD_ACCESS_KEY_ID', accessKeyId);
+  core.exportVariable('ALIBABACLOUD_ACCESS_KEY_SECRET', accessKeySecret);
+  core.exportVariable('ALIBABACLOUD_SECURITY_TOKEN', securityToken);
 }
 
 async function run() {
-    const roleArn = core.getInput('role-arn');
-    const oidcProviderArn = core.getInput('oidc-provider-arn');
-    const oidcTokenFilePath = core.getInput('oidc-provider-arn');
-    if (roleArn && oidcProviderArn && oidcTokenFilePath) {
-      const config = new Config({
-          type: 'oidc_role_arn',
-          roleArn,
-          oidcProviderArn,
-          oidcTokenFilePath,
-          roleSessionName: 'github-action-session'
-      });
-      const client = new CredentialClient(config);
-      const accessKeyId = await client.getAccessKeyId();
-      const accesskeySecret = await client.getAccessKeySecret();
-      const securityToken = await client.getSecurityToken();
-      setOutput(accessKeyId, accesskeySecret, securityToken);
-      return;
-    }
-
+  const roleToAssume = core.getInput('role-to-assume');
+  const oidcProviderArn = core.getInput('oidc-provider-arn');
+  if (roleToAssume && oidcProviderArn) {
+    const idToken = await core.getIDToken();
+    const oidcTokenFilePath = path.join(os.tmpdir(), 'token');
+    // write into token file
+    await fsx.writeFile(oidcTokenFilePath, idToken);
     const config = new Config({
-      type: 'ecs_ram_role'
+      type: 'oidc_role_arn',
+      roleArn: roleToAssume,
+      oidcProviderArn,
+      oidcTokenFilePath,
+      roleSessionName: ROLE_SESSION_NAME
     });
     const client = new CredentialClient(config);
     const accessKeyId = await client.getAccessKeyId();
     const accesskeySecret = await client.getAccessKeySecret();
     const securityToken = await client.getSecurityToken();
+    setOutput(accessKeyId, accesskeySecret, securityToken);
+    return;
+  }
 
-    const roleToAssume = core.getInput('role-to-assume');
-    if (roleToAssume) {
-        const config = new Config({
-            type: 'ram_role_arn',
-            accessKeyId,
-            accessKeySecret,
-            securityToken,
-            roleArn: roleToAssume,
-            roleSessionName: 'github-action-session'
-        });
-        const cred = new CredentialClient(config);
-        const accessKeyId = await cred.getAccessKeyId();
-        const accessKeySecret = await cred.getAccessKeySecret();
-        const securityToken = await cred.getSecurityToken();
-        setOutput(accessKeyId, accesskeySecret, securityToken);
-        return;
+  const config = new Config({
+    type: 'ecs_ram_role'
+  });
+  const client = new CredentialClient(config);
+  const accessKeyId = await client.getAccessKeyId();
+  const accessKeySecret = await client.getAccessKeySecret();
+  const securityToken = await client.getSecurityToken();
+
+  if (roleToAssume) {
+    const config = new Config({
+      type: 'ram_role_arn',
+      accessKeyId,
+      accessKeySecret,
+      securityToken,
+      roleArn: roleToAssume,
+      roleSessionName: ROLE_SESSION_NAME
+    });
+
+    {
+      const cred = new CredentialClient(config);
+      const accessKeyId = await cred.getAccessKeyId();
+      const accessKeySecret = await cred.getAccessKeySecret();
+      const securityToken = await cred.getSecurityToken();
+      setOutput(accessKeyId, accessKeySecret, securityToken);
     }
 
-    setOutput(accessKeyId, accesskeySecret, securityToken);
-
     return;
+  }
+
+  setOutput(accessKeyId, accessKeySecret, securityToken);
+
+  return;
 }
 
-run();
+if (require.main === require.cache[eval('__filename')]) {
+  run().catch((err) => {
+    core.setFailed(err.message);
+  });
+}
 
+exports.run = run;
 })();
 
 module.exports = __webpack_exports__;
