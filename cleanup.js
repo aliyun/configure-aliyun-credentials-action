3c4f74fa91a972bf236e6946294226949fcda842
Jackson Tian <shyvo1987@gmail.com>
2024-06-17 10:55:49 +0800

    set credentials security

--- cleanup.js
+++ cleanup.js
@@ -2,6 +2,11 @@
 
 const core = require('@actions/core');
 
+// use standard environment variables
+core.exportVariable('ALIBABA_CLOUD_ACCESS_KEY_ID', '');
+core.exportVariable('ALIBABA_CLOUD_ACCESS_KEY_SECRET', '');
+core.exportVariable('ALIBABA_CLOUD_SECURITY_TOKEN', '');
+// keep it for compatibility
 core.exportVariable('ALIBABACLOUD_ACCESS_KEY_ID', '');
 core.exportVariable('ALIBABACLOUD_ACCESS_KEY_SECRET', '');
 core.exportVariable('ALIBABACLOUD_SECURITY_TOKEN', '');
