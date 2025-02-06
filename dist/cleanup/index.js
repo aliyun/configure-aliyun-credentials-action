8ba8950b03960960c673b722fa0b8f9b8e874968
Jiang <lei.jiang@elsevier.com>
2025-02-06 21:11:29 +0800

    add terraform remote state oss support and multiple enhancements

--- dist/cleanup/index.js
+++ dist/cleanup/index.js
@@ -3594,11 +3594,12 @@ core.exportVariable('ALIBABA_CLOUD_SECURITY_TOKEN', '');
 core.exportVariable('ALIBABACLOUD_ACCESS_KEY_ID', '');
 core.exportVariable('ALIBABACLOUD_ACCESS_KEY_SECRET', '');
 core.exportVariable('ALIBABACLOUD_SECURITY_TOKEN', '');
-})();
 // keep it for terraform oss backend support https://developer.hashicorp.com/terraform/language/backend/oss
 core.exportVariable('ALICLOUD_ACCESS_KEY', '');
 core.exportVariable('ALICLOUD_SECRET_KEY', '');
 core.exportVariable('ALICLOUD_SECURITY_TOKEN', '');
+})();
+
 
 module.exports = __webpack_exports__;
 /******/ })()
